2ebeda4eb3ba874d64052251b69f23c63e1208cd
Trinity Cisneros <trinitycisneros@usc-guestwireless-upc-newsc2573.usc.edu>
2019-02-19 18:26:34 -0800

    surprise more changes

--- Instructions/starter_code/api_keys.py
+++
@@ -1,2 +0,0 @@
-# OpenWeatherMap API Key
-api_key = "34ac2115043eac022b374b864bd54427"
---
+++ Instructions/starter_code/config.py
@@ -0,0 +1,4 @@
+# OpenWeatherMap API Key
+om_key = "34ac2115043eac022b374b864bd54427"
+
+gkey = "AIzaSyDntaSif07bYLnpzSc0a_ahoszZsDf4Ezo"
